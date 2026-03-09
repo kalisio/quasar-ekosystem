c43c10021139e1523103ddf157b4c2bf93cdf6fb
cnouguier <ch.nouguier@gmail.com>
2026-03-09 09:02:24 +0100

    chore: updated vitest configuration and lint

---
+++ packages/quasar-form/vitest.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  root: __dirname,
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components')
+    }
+  },
+  test: {
+    name: 'quasar-form',
+    environment: 'happy-dom',
+    globals: true,
+    silent: false,
+    testTimeout: 30000,
+    include: ['test/**/*.test.js'],
+    coverage: {
+      provider: 'v8',
+      all: true,
+      clean: true,
+      include: ['/src/**/*.js'],
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: '../../coverage/quasar-form'
+    }
+  }
+})
