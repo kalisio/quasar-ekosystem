fe6067d5586042775d174047b1f40a5d25b09100
Sacha <sacha.colbert-lisbona@kalisio.com>
2026-04-30 19:12:44 +0200

    fix: Pass CI to analyse the monorepo on Sonar

---
+++ packages/quasar-moment/test/index.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import * as quasarMoment from '../src/index.js'
+
+describe('quasar-moment', () => {
+  it('exports a module', () => {
+    expect(quasarMoment).toBeDefined()
+  })
+})
