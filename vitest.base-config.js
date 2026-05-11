c3a077d553a62525a6ecca06c5ecb901df172eda
cnouguier <ch.nouguier@gmail.com>
2026-05-11 22:56:47 +0200

    chore: updated repo configuration

--- vitest.base-config.js
+++ vitest.base-config.js
@@ -1,8 +1,4 @@
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
 import { defineConfig } from 'vitest/config'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const baseConfig = defineConfig({
   test: {
