f566adb3506d379caccd4ed03ec7e5d4c1a9bab7
Sacha <sacha.colbert-lisbona@kalisio.com>
2026-05-12 11:09:48 +0200

    wip: implement QuasarCore in quasar-form components

--- packages/quasar-form/src/plugin.js
+++ packages/quasar-form/src/plugin.js
@@ -1,9 +1,11 @@
 import { defineAsyncComponent } from 'vue'
+import QuasarCore from '@kalisio/quasar-core'
 
 const modules = import.meta.glob('./components/**/*.vue')
 
 export const plugin = {
-  install (app) {
+  install (app, options = {}) {
+    app.use(QuasarCore, options)
     for (const [path, loader] of Object.entries(modules)) {
       const name = path.split('/').pop().replace('.vue', '')
       app.component(name, defineAsyncComponent(loader))
